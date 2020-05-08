68913da5fb7a344657899d7990b80039de091aee
Matthew Tang <matthewtang1999@gmail.com>
2020-05-08 18:43:40 -0400

    Disable search

--- src/app/course-search/course-search.component.ts
+++ src/app/course-search/course-search.component.ts
@@ -25,16 +25,16 @@ export class CourseSearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.courses$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
-      debounceTime(300),
+    // this.courses$ = this.searchTerms.pipe(
+    //   // wait 300ms after each keystroke before considering the term
+    //   debounceTime(300),
 
-      // ignore new term if same as previous term
-      distinctUntilChanged(),
+    //   // ignore new term if same as previous term
+    //   distinctUntilChanged(),
 
-      // switch to new search observable each time the term changes
-      switchMap((term: string) => this.courseService.searchCourse(term)),
-    );
+    //   // switch to new search observable each time the term changes
+    //   switchMap((term: string) => this.courseService.searchCourse(term)),
+    // );
   }
 
 }
